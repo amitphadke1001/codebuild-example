6c8cc797188e7888dc89cfd89aeef41bfc4765be
amitphadke1001 <68331991+amitphadke1001@users.noreply.github.com>
2020-08-18 20:25:28 +0530

    Update Converter.php

--- app/Converter.php
+++ app/Converter.php
@@ -4,5 +4,9 @@ namespace SitePoint\Converter;
 
 class Converter
 {
-   
+   public function convertString(string $input): ?array
+    {
+        $output = json_decode($input, true);
+        return $output;
+    }
 }
